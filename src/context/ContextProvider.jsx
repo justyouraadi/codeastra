69030b9271d5ee91410e3e14a6525e675ba7a198
VK-Runthla <khandalvashisth@gmail.com>
2025-12-22 20:44:26 +0530

    commiting changes

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,15 +1,15 @@
 import React, { createContext, useContext } from "react";
-import { useAuthProvider } from "../hooks/useAuthProvider"; // ✅ tumhara custom hook
+import { useAuthProvider } from "../hooks/useAuthProvider";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-const auth = useAuthProvider();
-return (
-<AuthContext.Provider value={auth}>
-{children}
-</AuthContext.Provider>
-);
+    const auth = useAuthProvider();
+    return (
+        <AuthContext.Provider value={auth}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export const useAuth = () => useContext(AuthContext);
